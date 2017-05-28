ed166f51ce4714e9748404279a6f52913537af5f
nablam <lamribenn@gmail.com>
2017-05-28 11:56:15 -0400

    using the DoFullConversion before it was implemented

--- src/Person.java
+++ src/Person.java
@@ -4,11 +4,22 @@ public class Person {
 
 	public String firstName, lastName;
 	public double amountOfMoney;
-	
+	private Validator _vlidator;
+	private Number _num;
 	public Person() {
 		firstName = "first";
 		lastName = "last";
 		amountOfMoney = 5000.00;
+		_vlidator = new Validator(amountOfMoney);
+		_num= new Number();
 	}
+	
+	public String StringTobeWrittenOnCheck(double argAmunt){
+		if (_vlidator.validate(argAmunt)){
+			return _num.DoFullConversion(argAmunt);	
+		}
+		return "not enough money";
+	}
+	
 
 }
